8adb6ae0222f67c36d0b5da6e5335d84a3af63f7
RambabuMS <raambabu96@gmail.com>
2022-03-18 23:22:07 +0530

    node-app-2 created

---
+++ index.js
@@ -0,0 +1,77 @@
+// const express = require("express");
+import express from "express";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+
+const PORT = 4000;
+
+app.use(express.json());
+
+const MONGO_URL = process.env.MONGO_URL;
+async function createConnection() {
+  const client = new MongoClient(MONGO_URL);
+  await client.connect();
+  console.log("Mongo is connected ✌️😊");
+  return client;
+}
+const client = await createConnection();
+
+app.get("/", function (req, res) {
+  res.send("Welcome Home");
+});
+
+// app.get("/cars", function (req, res) {
+//   res.send(Carslist);
+// });
+app.get("/cars", async function (req, res) {
+  const cars = await client.db("b30wd").collection("cars").find({}).toArray();
+  res.send(cars);
+});
+
+app.get("/cars/:id", async function (req, res) {
+  console.log(req.params);
+  const { id } = req.params;
+  //   const car = Carslist.find((car) => car.id === id);
+  const car = await client.db("b30wd").collection("cars").findOne({ id: id });
+  car
+    ? res.send(car)
+    : res.status(404).send({ message: "No such car found 😅" });
+});
+
+app.post("/cars", async function (req, res) {
+  const data = req.body;
+  const cars = await client.db("b30wd").collection("cars").insertMany(data);
+
+  res.send(cars);
+});
+
+app.put("/cars/:id", async function (req, res) {
+  console.log(req.params);
+  const { id } = req.params;
+  //   const car = Carslist.find((car) => car.id === id);
+  const updatedCar = req.body;
+  const result = await client
+    .db("b30wd")
+    .collection("cars")
+    .updateOne({ id: id }, { $set: updatedCar });
+  res.send(result);
+});
+
+app.delete("/cars/:id", async function (req, res) {
+  console.log(req.params);
+  const { id } = req.params;
+  //   const car = Carslist.find((car) => car.id === id);
+  const result = await client
+    .db("b30wd")
+    .collection("cars")
+    .deleteOne({ id: id });
+  res.send(result);
+});
+
+app.listen(PORT, () => {
+  console.log("Server is started");
+});
+
+// const { id } = request.params;const movie = movies.find((mv) => mv.id === id);response.send(movie);});
