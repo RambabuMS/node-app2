751a761b846fa2341ae9e9fd9467fe4c909cbb7e
RambabuMS <raambabu96@gmail.com>
2022-03-18 23:30:24 +0530

    heroku steps

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
-const PORT = 4000;
-
+const PORT = process.env.PORT;
 app.use(express.json());
 
 const MONGO_URL = process.env.MONGO_URL;
